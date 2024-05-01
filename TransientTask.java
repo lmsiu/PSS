76ab2ddb9bac38d0530a63e3acccd29b743e411c
Nishat Quyoum <nishatq9006@gmail.com>
2024-04-30 17:18:41 -0700

    added super class

---
+++ TransientTask.java
@@ -0,0 +1,8 @@
+public class TransientTask extends Task {
+    // Constructor for TransientTask
+    public TransientTask(String name, TypeCategory typeCategory, float startTime, float duration, int date) {
+        // Call the superclass constructor using super()
+        super(name, typeCategory, startTime, duration, date);
+    }
+
+}
