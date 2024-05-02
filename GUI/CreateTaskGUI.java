4086960b4e8faaa0b2f25068303b5cb0aee5a986
BuildTools <unconfigured@null.spigotmc.org>
2024-05-01 22:56:11 -0700

    Made formatting adjustments to both of the GUI classes. Also added a method to choose a file and check if it's a JSON filetype

--- GUI/CreateTaskGUI.java
+++ GUI/CreateTaskGUI.java
@@ -1,50 +1,65 @@
 package GUI;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
 
 public class CreateTaskGUI {
 
     // return the JFrame for the create task screen
     public JFrame getCreateTaskScreen(){
-        JFrame frame = new JFrame();
-
-        // text
+        // Adjusting the JFrame
+        JFrame frame = new JFrame("PSS");
+        frame.setSize(700, 400);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+        // Panel within the frame to hold the text and buttons
+        JPanel panel = new JPanel( );
+        panel.setPreferredSize(new java.awt.Dimension (550,300));
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setBorder(BorderFactory.createLineBorder(Color.BLACK,5));
+        panel.setBackground(new Color(245, 245, 255));
+        // Creating the text and aligning it in a certain format
         JLabel createTaskText = new JLabel("What type of task would you like to create?");
-        // buttons
+        createTaskText.setAlignmentX(Component.CENTER_ALIGNMENT);
+        createTaskText.setPreferredSize(new Dimension(350, 100));
+        createTaskText.setFont(new Font("Dialog", Font.BOLD, 20));
+        createTaskText.setForeground(new Color(140,155, 255));
+        // Creating the buttons and aligning them
         JButton transientTaskButton = makeButton("Transient Task");
         JButton recurringTaskButton = makeButton("Recurring Task");
         JButton antiTaskButton = makeButton("Anti Task");
+        transientTaskButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        recurringTaskButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        antiTaskButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        JPanel panel = new JPanel( );
-        panel.setLayout(new BoxLayout(panel,  BoxLayout.Y_AXIS));
 
+        // Adding each component to the panel and the panel to the frame
+        panel.add(Box.createVerticalStrut(20));
         panel.add(createTaskText);
+        panel.add(Box.createVerticalStrut(40));
         panel.add(transientTaskButton);
+        panel.add(Box.createVerticalStrut(20));
         panel.add(recurringTaskButton);
+        panel.add(Box.createVerticalStrut(20));
         panel.add(antiTaskButton);
 
         frame.add(panel);
 
-        // 400 width and 500 height
-        frame.setSize(500, 600);
+
+
 
         return frame;
 
     }
-
     // create button method to ensure uniform buttons are created
     private JButton makeButton(String text){
         JButton button = new JButton(text);
- 
+
         // x axis, y axis, width, height
         button.setBounds(150, 200, 220, 50);
 
         return button;
 
     }
-    
+
 }
--- GUI/PSSGUI.java
+++ GUI/PSSGUI.java
@@ -1,34 +1,51 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.JFileChooser;
+
 
 
 public class PSSGUI {
+    JButton viewScheduleButton;
+    JButton createTaskButton;
+    JDialog testDialog;
+
     public static void main(String args[]){
         // Creating instance of JFrame
         JFrame frame = new JFrame();
-
-        JLabel createTaskText = new JLabel("What would you like to do?");
-
-        // Creating instance of JButton
+        frame.setSize(600, 400);
+        frame.setLayout(new FlowLayout());
+        // Creating Prompt text
+        JLabel createTaskText = new JLabel("What would you like to do?", SwingConstants.CENTER);
+        createTaskText.setPreferredSize(new Dimension(350, 50));
+        createTaskText.setFont(new Font("Dialog", Font.BOLD, 18));
+        // Creating instance of JButton and setting their dimensions
         JButton createTaskButton = makeButton(" Create a Task");
         JButton viewScheduleButton = makeButton("View Schedule");
- 
-        // x axis, y axis, width, height
-        createTaskButton.setBounds(150, 200, 220, 50);
- 
-        // adding button
-        JPanel panel = new JPanel( );
-        panel.setLayout(new BoxLayout(panel,  BoxLayout.Y_AXIS));
 
+        // Panel setup
+        JPanel panel = new JPanel( );
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 40, 10));
+        panel.setPreferredSize(new java.awt.Dimension(300,200));
+        panel.setBackground(new Color(150, 155, 230));
+        panel.setBorder(BorderFactory.createLineBorder(Color.BLACK,5));
+        // Adding Text and Buttons to the panel
         panel.add(createTaskText);
         panel.add(createTaskButton);
         panel.add(viewScheduleButton);
 
         frame.add(panel);
- 
-        // 400 width and 500 height
-        frame.setSize(500, 600);
+
+        viewScheduleButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                viewScheduleButtonAction(ae);
+            }
+        });
+
 
         // making the frame visible
         frame.setVisible(true);
@@ -40,12 +57,29 @@ public class PSSGUI {
             frame.setVisible(false);
             createTaskFrame.setVisible(true);
         });
-    }
 
+    }
+    // Prompts user to choose a file from the user's system
+    private static void viewScheduleButtonAction(ActionEvent ae) {
+        JFileChooser chooseFile = new JFileChooser();
+        int check = chooseFile.showOpenDialog(null);
+        if(check == JFileChooser.APPROVE_OPTION){
+            // Stores the file, the filename, and the filetype
+            File scheduleFile = chooseFile.getSelectedFile();
+            String filename = scheduleFile.getAbsolutePath();
+            String filetype = chooseFile.getTypeDescription(scheduleFile);
+            System.out.println(filetype);
+
+            // Verifies that the file is of JSON format
+            if (filetype.equals("JSON File")){
+                System.out.println("This file is verified as a JSON File");
+            }
+        }
+    }
     // create button method to ensure uniform buttons are created
     private static JButton makeButton(String text){
         JButton button = new JButton(text);
- 
+
         // x axis, y axis, width, height
         button.setBounds(150, 200, 220, 50);
 
