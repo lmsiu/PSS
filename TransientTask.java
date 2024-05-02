7aaa48f1a71aee5788e92282b94aa950faf67d81
Nishat Quyoum <nishatq9006@gmail.com>
2024-05-01 22:23:47 -0700

    added subclasses for transit task

--- TransientTask.java
+++ TransientTask.java
@@ -1,8 +1,41 @@
+import java.util.*;
+
 public class TransientTask extends Task {
+    private String name;
+    private TypeCategory typeCategory;
+    private float startTime;
+    private float duration;
+    private int date;
+
     // Constructor for TransientTask
-    public TransientTask(String name, TypeCategory typeCategory, float startTime, float duration, int date) {
-        // Call the superclass constructor using super()
-        super(name, typeCategory, startTime, duration, date);
+    TransientTask (String name, TypeCategory typeCategory, float startTime, float duration, int date) {
+        this.name = name;
+        setTaskType(typeCategory);
+        this.startTime = startTime;
+        this.duration = duration;
+        this.date = date;
+    }
+
+    // Set the Transient Task type (Appointment, Shopping, and Visit)
+    public void setTaskType(TypeCategory transientTaskType) {
+        if (transientTaskType != TypeCategory.APPOINTMENT &&
+            transientTaskType != TypeCategory.SHOPPING &&
+            transientTaskType != TypeCategory.VISIT) {
+            throw new IllegalArgumentException("Invalid task type. Must be one of: APPOINTMENT, SHOPPING, VISIT");
+        }
+        this.transientTaskType = transientTaskType;
+    }
+
+    // Get the Transient Task type
+    public TypeCategory getTaskType() {
+        return this.typeCategory;
+    }
+
+    // enum declaration for Type Categories for Transient Task
+    public enum TypeCategory {
+        APPOINTMENT,
+        SHOPPING,
+        VISIT
     }
 
 }
