70d36d3d2ebdf77700ef832b79e6de94671982b4
Nishat Quyoum <nishatq9006@gmail.com>
2024-05-15 22:06:07 -0700

    Edited antitask method in controller

--- Tasks/Controller.java
+++ Tasks/Controller.java
@@ -35,20 +35,22 @@ public class Controller {
 
         return null;
     }
-
-    // Anti Task
-    public Exception createAntiTask(String name,  int startTimeMinute, int startTimeHour, boolean AM, int durationHour, int durationMinutes, int dateYear, int dateMonth, int dateDay){
-
-        // returns the exception so it can be printed to the screen if something goes wrong
-        try {
-            AntiTask newTask = new AntiTask(name, startTimeMinute, startTimeHour, AM, durationHour, durationMinutes, dateYear, dateMonth, dateDay);
-            model.createTask(newTask);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return e;
-        }
-        return null;
+    
+ // Create AntiTask method
+ public void createAntiTask(String name, int startTimeMinute, int startTimeHour, boolean AM, int durationHour, int durationMinutes, int dateYear, int dateMonth, int dateDay) throws Exception {
+    RecurringTask correspondingRecurringTask = AntiTask.findCorrespondingRecurringTask(model, startTimeHour, startTimeMinute, durationHour, durationMinutes, dateYear, dateMonth, dateDay);
+    if (correspondingRecurringTask == null) {
+        throw new Exception("Cannot create anti-task because there is no corresponding recurring task!");
     }
+    try {
+        AntiTask newTask = new AntiTask(name, startTimeMinute, startTimeHour, AM, durationHour, durationMinutes, dateYear, dateMonth, dateDay);
+        model.createTask(newTask);
+    } catch (Exception e) {
+        e.printStackTrace();
+        throw e;
+    }
+}
+    
 
 
     // Delete Task
